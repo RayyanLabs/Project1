8c72972e38cba3eb5f63caccf9b812c2d000d4a6
Rayyan1222 <156992065+Rayyan1222@users.noreply.github.com>
2025-03-06 01:39:39 -0600

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 document.getElementById('content').addEventListener('submit', function(event) {
-  var checkbox = document.getElementById('is-lebron-james-the-goat');
+  var checkbox = document.getElementById('Is-Lebron-James-The-Goat');
   if (!checkbox.checked) {
     event.preventDefault();
     alert('You Must Accept Lebron Is The Goat To Join The Club!');
