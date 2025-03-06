79e37f8b0a08748a84be99c39b03acdd4b64ca7f
Rayyan1222 <156992065+Rayyan1222@users.noreply.github.com>
2025-03-06 01:17:12 -0600

    javascript to check if username entered or not

--- js/site.js
+++ js/site.js
@@ -1,1 +1,9 @@
-
+document.getElementById('content').addEventListener('submit', function(event) {
+  var username = document.getElementById('username').value;
+  if (username === '') {
+    event.preventDefault();
+    alert('Please enter your username');
+  } else {
+    alert("Congratulations on Joining the LeBron SuperFan Club!");
+    }
+  });
